c921e738703630573e181a5f80d4ea6b26d70f26
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2020-12-04 22:32:30 -0800

    cooler

--- day5.py
+++ day5.py
@@ -4,16 +4,14 @@ data = get_data(day=5).split("\n")
 data = list(map(lambda x: x.strip(), data))
 
 used = set()
-translation = {
-    ord("F"): ord("0"),
-    ord("L"): ord("0"),
-    ord("B"): ord("1"),
-    ord("R"): ord("1"),
-}
 
+t = str.maketrans("FBLR", "0101")
 for e in data:
-    b = e.translate(translation)
+    b = e.translate(t)
     used.add(int(b, 2))
 
 print(max(used))
-print(list(set(range(min(used), max(used))).difference(used))[0])
+print(*(set(range(min(used), max(used))) - used))
+
+# one liner for fun
+# (lambda d: print(max(d), *set(range(min(d), max(d))) - d))(set([int(e.translate(str.maketrans("FBLR", "0101")), 2) for e in get_data(day=5).split("\n")]))
