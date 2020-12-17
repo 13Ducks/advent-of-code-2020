2614dffe1725175b9cb877743e1ab293d237414b
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2020-12-16 23:54:50 -0800

    optimized solution

--- day17.py
+++ day17.py
@@ -1,59 +1,50 @@
 from aocd import get_data
 from collections import defaultdict
+import itertools
 
 data = get_data(day=17).split("\n")
-nx, ny = len(data[0]), len(data)
 
 
-def initial_grid(data):
-    grid = defaultdict(lambda: 0)
-    for i, r in enumerate(data):
-        for j in range(len(r)):
-            if r[j] == "#":
-                grid[(j, i, 0, 0)] = 1
-    return grid
+def init_state():
+    state = [
+        (i, j, 0, 0)
+        for i, r in enumerate(data)
+        for j, state in enumerate(r)
+        if state == "#"
+    ]
+    return state
 
 
-def count_neighbors(x, y, z, w, grid):
-    active = 0
-    for dx in range(-1, 2):
-        for dy in range(-1, 2):
-            for dz in range(-1, 2):
-                for dw in range(-1, 2):
-                    if (dx, dy, dz, dw) != (0, 0, 0, 0) and grid[
-                        (x + dx, y + dy, z + dz, w + dw)
-                    ] == 1:
-                        active += 1
-    return active
+def p1(state):
+    neighbors = defaultdict(lambda: 0)
+    for x, y, z, w in state:
+        for dx, dy, dz in itertools.product([-1, 0, 1], repeat=3):
+            if (dx, dy, dz) != (0, 0, 0):
+                neighbors[(x + dx, y + dy, z + dz, w)] += 1
+    return [
+        s for s in neighbors if neighbors[s] == 3 or (s in state and neighbors[s] == 2)
+    ]
 
 
-def cycle(grid, n, p1):
-    if p1:
-        min_w, max_w = 0, 1
-    else:
-        min_w, max_w = -n - 1, n + 2
-    new_grid = defaultdict(lambda: 0)
-    for x in range(-n - 1, nx + n + 1):
-        for y in range(-n - 1, ny + n + 1):
-            for z in range(-n - 1, n + 2):
-                for w in range(min_w, max_w):
-                    p = (x, y, z, w)
-                    num_neighbors = count_neighbors(*p, grid)
-                    if grid[p] == 0 and num_neighbors == 3:
-                        new_grid[p] = 1
-                    if grid[p] == 1 and (num_neighbors in (2, 3)):
-                        new_grid[p] = 1
-    return new_grid
+def p2(state):
+    neighbors = defaultdict(lambda: 0)
+    for x, y, z, w in state:
+        for dx, dy, dz, dw in itertools.product([-1, 0, 1], repeat=4):
+            if (dx, dy, dz, dw) != (0, 0, 0, 0):
+                neighbors[(x + dx, y + dy, z + dz, w + dw)] += 1
+    return [
+        s for s in neighbors if neighbors[s] == 3 or (s in state and neighbors[s] == 2)
+    ]
 
 
-grid = initial_grid(data)
+state = init_state()
 for i in range(6):
-    grid = cycle(grid, i, True)
+    state = p1(state)
 
-print(list(grid.values()).count(1))
+print(len(state))
 
-grid = initial_grid(data)
+state = init_state()
 for i in range(6):
-    grid = cycle(grid, i, False)
+    state = p2(state)
 
-print(list(grid.values()).count(1))
+print(len(state))
